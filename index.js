2e05db7e789646cbc763ba479f524eeb6527cdd8
firozahmmed1 <firozahmmed05@gmail.com>
2023-05-04 13:17:38 +0600

    fifth-update

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const data = require('./chef-data.json');
+
 app.get('/', (req, res)=>{
     res.send('Hello Server');
+})
+
+app.get('/data', (req, res)=>{
+    res.send(data);
+})
+
+app.get('/data/:id', (req, res) =>{
+    const id = req.params.id;
+    const singleData = data.find(sd => sd._id === id);
+    res.send(singleData);
 })
 
 app.listen(port, ()=>{
