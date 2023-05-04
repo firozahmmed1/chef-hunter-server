32e5a82f8caf173d6ca24ddbf250d85054b6370a
firozahmmed1 <firozahmmed05@gmail.com>
2023-05-04 13:00:03 +0600

    third-update

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get('/', (req, res)=>{
+    res.send('Hello Server');
+})
 
+app.listen(port, ()=>{
+         console.log(`Server is running: ${port}`);
+})
